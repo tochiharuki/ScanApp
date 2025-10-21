0a410895ad945bf28eafda9f0d647da7536da60f
Haruki Tochishita <tochi.haruki@gmail.com>
2025-10-22 00:41:10 +0900

    all

--- ScanApp/ScanAppApp.swift
+++ ScanApp/ScanAppApp.swift
@@ -9,9 +9,22 @@ import SwiftUI
 
 @main
 struct ScanAppApp: App {
+    init() {
+        // アプリ全体のナビゲーションバーやタブの色を設定したい場合
+        UINavigationBar.appearance().backgroundColor = .white
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.black]
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.black]
+        
+        UITabBar.appearance().backgroundColor = .white
+        UITabBar.appearance().tintColor = .black  // 選択中のアイコン色
+        UITabBar.appearance().unselectedItemTintColor = .gray
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .accentColor(.black) // ボタンなどのアクセントカラー
+                .preferredColorScheme(.light) // 常にライトモード
         }
     }
 }
