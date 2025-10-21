ef5a613008b99b6f07b656697fbdda5f03204aa5
Haruki Tochishita <tochi.haruki@gmail.com>
2025-10-21 22:16:16 +0900

    all

--- ScanApp/FileListView.swift
+++ ScanApp/FileListView.swift
@@ -5,4 +5,16 @@
 //  Created by Tochishita Haruki on 2025/10/21.
 //
 
-import Foundation
+import SwiftUI
+
+struct FileListView: View {
+    var body: some View {
+        NavigationView {
+            List {
+                Text("No scanned documents yet.")
+                    .foregroundColor(.secondary)
+            }
+            .navigationTitle("Files")
+        }
+    }
+}
