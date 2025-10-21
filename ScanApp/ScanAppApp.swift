587e413d0d8ae191b393f11dfb3a7ff8cea02a22
Haruki Tochishita <tochi.haruki@gmail.com>
2025-10-21 20:51:21 +0900

    Initial Commit

---
+++ ScanApp/ScanAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanAppApp.swift
+//  ScanApp
+//
+//  Created by Tochishita Haruki on 2025/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ScanAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
