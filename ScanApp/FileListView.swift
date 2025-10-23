856f647c10982b85a67ce1f4b47b2aaeea3d6e81
Haruki Tochishita <tochi.haruki@gmail.com>
2025-10-23 12:09:57 +0900

    all

--- ScanApp/FileListView.swift
+++ ScanApp/FileListView.swift
@@ -65,13 +65,11 @@ struct FileListView: View {
                         Image(systemName: "folder.badge.plus")
                     }
                     // ✅ ファイル移動
-                    if isEditing && !selectedFiles.isEmpty {
-                        Button {
+                    Button {
                             showMoveSheet = true
                         } label: {
                             Image(systemName: "arrow.right.folder")
                         }
-                    }
 
 
                     // 削除ボタン
