097ea439762c46270c56bde3bbfb255d02fea71e
Haruki Tochishita <tochi.haruki@gmail.com>
2025-10-24 22:11:58 +0900

    all

--- ScanApp/FileListView.swift
+++ ScanApp/FileListView.swift
@@ -3,19 +3,6 @@ import Foundation
 
 struct FileListView: View {
     @State private var currentURL: URL
-
-    init(currentURL: URL? = nil) {
-        _currentURL = State(initialValue:
-            currentURL ?? FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
-        )
-    }
-
-    var body: some View {
-        import SwiftUI
-import Foundation
-
-struct FileListView: View {
-    @State private var currentURL: URL
     @State private var isLoading = false
 
     init(currentURL: URL? = nil) {
