8a8acd1399d47498b2e5e0821025fe27ac549894
Haruki Tochishita <tochi.haruki@gmail.com>
2025-10-24 12:02:01 +0900

    all

--- ScanApp/PathBarView.swift
+++ ScanApp/PathBarView.swift
@@ -37,11 +37,18 @@ struct PathBarView: View {
         var paths: [URL] = []
         var current = currentURL
         let documents = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
-        while current.path != documents.deletingLastPathComponent().path {
+    
+        // セーフティカウンターで無限ループ回避（念のため）
+        var safetyCounter = 0
+        while current.path.hasPrefix(documents.path) {
             paths.insert(current, at: 0)
             if current == documents { break }
             current.deleteLastPathComponent()
+    
+            safetyCounter += 1
+            if safetyCounter > 50 { break } // 異常時の緊急脱出
         }
+    
         return paths
     }
 }
