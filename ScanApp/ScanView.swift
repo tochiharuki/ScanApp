7c36c813414e6f4725e9008aaf8f8d0107b5d6ce
Haruki Tochishita <tochi.haruki@gmail.com>
2025-10-25 16:45:50 +0900

    all

--- ScanApp/ScanView.swift
+++ ScanApp/ScanView.swift
@@ -97,6 +97,10 @@ struct ScanView: View {
                             .padding(.horizontal)
                     }
                 }
+                .padding()
+                .background(Color(.systemGray6))
+                .cornerRadius(12)
+                .shadow(color: Color.black.opacity(0.1), radius: 4, x: 0, y: 2)
                 .padding(.horizontal, 40)
 
             }
